a4fb045c1b053eaa07ade7fdccafbb4f3ec04c1f
OshinoLi <leaaachy@gmail.com>
2022-03-15 14:45:06 +0800

    countdown, reset, play sound

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 const app = Vue.createApp({
     data() {
+        // const sound = require('@/sound.mp3')
         return {
-            minute: 45,
-            second: 00,
+            // minute: 45,
+            // second: 00,
+            minutes: 00,
+            secondes: 05,
+            countdown: 45*60 + 0,
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg',
-            timer: null
-
+            timer: null,
+            audio: new Audio('https://cloud.ccllssd.com/wl/?id=gFVFeIaYd5Gj6xfS00LhGK7LSLFtd2OY&fmode=download')
+            // audio: new Audio(this.sound)
         }
     },
-    mounted: function () {
-        sum = this.minute * 60 + this.second
-        this.timer = setInterval(() => {
-            if (sum == 0) { return }
-            sum -= 1
-            this.second = sum % 60
-            this.minute = (sum - this.second) / 60
-        }, 1)
-    },
-    beforeDestroy() {
-        clearInterval(this.timer)
-    },
-    // methods: {
-    //     async getuser() {
-    //         const res = await fetch('https://randomuser.me/api')
-    //         const { results } = await res.json()
-        
-    //         console.log(results)
-
-    //         this.minute = results[0].name.first
-    //         this.second = results[0].name.last
-    //         this.email = results[0].email
-    //         this.gender = results[0].gender
-    //         this.picture = results[0].picture.large
-    //     },
-    // }
+    
+    methods: {
+        start () {
+            this.countdown = this.minutes * 60 + this.secondes
+            this.timer = setInterval(() => {
+                if (this.countdown == 0) {
+                    this.audio.play()
+                    clearInterval(this.timer)
+                    // this.reset()
+                    return 
+                }
+                this.countdown -= 1
+                this.secondes = this.countdown % 60
+                this.minutes = (this.countdown - this.secondes) / 60
+            }, 1000)
+        },
+        stopPlay () {
+            this.reset()
+        },
+        reset () {
+            this.minutes = 00,
+            this.secondes = 05,
+            this.countdown = this.minutes*60 + this.secondes,
+            this.timer = null,
+            this.audio.pause()
+        },
+        beforeDestroy() {
+            clearInterval(this.timer)
+        },
+    }
 });
 
 app.mount("#app")
